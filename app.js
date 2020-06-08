2b253968c7d1de8ee5c1428dcc9435dea0a9dfee
Hoang <hoang@drobebox.com>
2020-06-08 11:34:25 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const { chromium } = require("playwright");
+const fs = require("fs");
+const { Parser } = require("json2csv");
+const fields = [
+  { value: "path", label: "PATH" },
+  { value: "method", label: "METHOD" },
+  { value: "body", label: "BODY" },
+  { value: "response", label: "RESPONSE" }
+];
+const json2csvParser = new Parser({ fields });
+
+(async () => {
+  const apiArr = [];
+  const browser = await chromium.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto("https://app.flood.io/");
+
+  page.on("requestfinished", async request => {
+    const url = request.url();
+    const urlPrefix = "https://api.flood.io";
+    if (url.includes(urlPrefix)) {
+      apiArr.push({
+        path: url.replace(urlPrefix, ""),
+        method: request.method(),
+        body: request.postData(),
+        response: JSON.stringify(
+          await request.response().then(res => res.json())
+        )
+      });
+      console.log(`captured: ${apiArr.length}`);
+    }
+  });
+
+  page.on("close", async () => {
+    fs.writeFileSync("./file.csv", json2csvParser.parse(apiArr));
+  });
+})();
